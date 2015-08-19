1af41a56375dc41b269edf0f1dad02a513fcb1e2
Terry <jinkeu@163.com>
2015-08-19 17:52:31 +0800

    Add new Project

---
+++ JKVideoPlayer/JKVideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKVideoPlayer
+//
+//  Created by mac on 15/8/19.
+//  Copyright (c) 2015年 uupye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JKVideoPlayer/JKVideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JKVideoPlayer
+//
+//  Created by mac on 15/8/19.
+//  Copyright (c) 2015年 uupye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
